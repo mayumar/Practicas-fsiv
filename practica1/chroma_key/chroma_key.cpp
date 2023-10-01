3b08aeacc978581b257c5c6956472760b1f2ed42
mayumar <maytalbada@gmail.com>
2023-10-01 13:37:40 +0200

    Practica 1 casi terminada

--- practica1/chroma_key/chroma_key.cpp
+++ practica1/chroma_key/chroma_key.cpp
@@ -41,8 +41,12 @@ main(int argc, char * argv[])
         }
 
         // Get the values
-
-
+        int hue = parser.get<int>("h");
+        int sens = parser.get<int>("s");
+        bool video = parser.get<bool>("v");
+        bool camera = parser.get<bool>("c");
+        cv::String input = parser.get<cv::String>("@input"); 
+        cv::String background = parser.get<cv::String>("@background");
         //
 
         if (!parser.check())
@@ -55,9 +59,91 @@ main(int argc, char * argv[])
 
         //DO the work!!.
 
+        cv::Mat backg = cv::imread(background, cv::IMREAD_ANYCOLOR);
 
+        if(backg.empty()){
+            std::cerr << "Error: no se ha podido abrir el fichero " << background << std::endl;
+            return EXIT_FAILURE;
+        }
 
+        if(!video && !camera){
 
+            cv::Mat foreg = cv::imread(input, cv::IMREAD_ANYCOLOR);
+
+            if(foreg.empty()){
+                std::cerr << "Error: no se ha podido abrir el fichero " << input << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            cv::Mat res = fsiv_apply_chroma_key(foreg, backg, hue, sens);
+
+            cv::namedWindow("IMG", cv::WINDOW_GUI_EXPANDED);
+
+            cv::imshow("IMG", res);
+
+            std::cout << "Pulsa ESC para salir" << std::endl;
+            while((cv::waitKey(0) & 0xff) != 27);
+
+            cv::destroyWindow("IMG");
+
+        }else if(!camera){
+
+            cv::VideoCapture vidforg;
+            vidforg.open(input);
+
+            if(!vidforg.isOpened()){
+                std::cerr << "Error: no he podido abrir el video " << input << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            cv::namedWindow("VIDEO");
+
+            int key = cv::waitKey(0) & 0xff;
+            cv::Mat frame;
+            vidforg >> frame;
+
+            while(!frame.empty() && key!=27){
+                cv::Mat output = fsiv_apply_chroma_key(frame, backg, hue, sens);
+                cv::imshow("VIDEO", output);
+
+                key = cv::waitKey(25) & 0xff;
+                vidforg >> frame;
+            }
+
+            cv::destroyWindow("VIDEO");
+
+        }else{
+
+            cv::VideoCapture camforg;
+            camforg.open(std::stoi(input));
+
+            if(!camforg.isOpened()){
+                std::cerr << "Error: no se ha podido abrir la fuente de video" << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            cv::namedWindow("VIDEO");
+
+            int key = cv::waitKey(0) & 0xff;
+            cv::Mat frame;
+            camforg >> frame;
+
+            if(frame.empty()){
+                std::cerr << "Error: no se ha podido abrir la fuente de video" << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            while(!frame.empty() && key!=27){
+                cv::Mat output = fsiv_apply_chroma_key(frame, backg, hue, sens);
+                cv::imshow("VIDEO", output);
+
+                key = cv::waitKey(25) & 0xff;
+                camforg >> frame;
+            }
+
+            cv::destroyWindow("VIDEO");
+
+        }
 
     }
     catch (std::exception& e)
