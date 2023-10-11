7fc519c9ed8047d3d1ab1136544cd816c88b42c1
mayumar <maytalbada@gmail.com>
2023-10-11 14:01:40 +0200

    Practica 2 empezada

--- practica2/img_equalization/common_code.cpp
+++ practica2/img_equalization/common_code.cpp
@@ -12,7 +12,11 @@ fsiv_compute_histogram(const cv::Mat& in, cv::Mat& hist)
     //1- Implementar un recorrido por la imagen y calcular el histograma.
     //2- Usar la función cv::calcHist.
     //Sugerencia: implementa las dos para comparar.
-
+    
+    int hist_size = 256;
+    float channel[] = {0, 256};
+    const float * ranges[] = {channel};
+    cv::calcHist(&in, 1, 0, cv::Mat(), hist, 1, &hist_size, ranges);
 
     //
 
@@ -29,6 +33,7 @@ fsiv_normalize_histogram(cv::Mat& hist)
 
     //TODO
 
+    cv::normalize(hist, hist, 1.0, 0.0, cv::NORM_L1);
 
     //
     CV_Assert(hist.type()==CV_32FC1);
@@ -48,6 +53,9 @@ fsiv_accumulate_histogram(cv::Mat& hist)
 
     //TODO
 
+    for(int i = 0; i < 256; i++){
+        hist.at<float>(i) += hist.at<float>(i - 1); //Sumas el anterior
+    }
 
     //
 
@@ -68,9 +76,15 @@ fsiv_create_equalization_lookup_table(const cv::Mat& hist,
     //Usa las funciones fsiv_normalize_histogram y fsiv_accumulate_histogram
     //para construir la tabla.
 
+    if(!hold_median){
+        cv::Mat naHist;
+        hist.copyTo(naHist);
 
+        fsiv_normalize_histogram(naHist);
+        fsiv_accumulate_histogram(naHist);
 
-
+        naHist.convertTo(lkt, CV_8UC1, 255, 0);
+    }
 
     //
 
@@ -91,7 +105,7 @@ fsiv_apply_lookup_table(const cv::Mat&in, const cv::Mat& lkt,
 
     //TODO
     //Hint: you can use the cv::LUT function.
-
+    cv::LUT(in, lkt, out);
     //
     CV_Assert(out.rows ==in.rows && out.cols==in.cols && out.type()==in.type());
     return out;
@@ -107,7 +121,29 @@ fsiv_image_equalization(const cv::Mat& in, cv::Mat& out,
     //fsiv_create_equalization_lookup_table y fsiv_apply_lookup_table
     //Recuerda: cuando radius>0, la zona no procesada de la entrada se deja
     //   con los mismos valores en la salida.
+    cv::Mat hist;
+    fsiv_compute_histogram(in, hist);
+    
+    if(radius == 0){
+        cv::Mat lut = fsiv_create_equalization_lookup_table(hist, hold_median);
+        fsiv_apply_lookup_table(in, lut, out);
+    }else{
 
+        for(int x = 0; x < (in.rows-2*radius+1); x++){
+            for(int y = 0; y < (in.rows-2*radius+1); y++){
+                cv::Range rangex(x, x+2*radius+1);
+                cv::Range rangey(y, y+2*radius+1);
+                cv::Mat ventana = in(rangex, rangey);
+
+                cv::Mat lut = fsiv_create_equalization_lookup_table(ventana, hold_median);
+
+                float res = lut.at<float>(in.at<float>(x+radius, y+radius), 0);
+
+                out.at<float>(x+radius, y+radius) = res;
+                
+            }
+        }
+    }
     //
     CV_Assert(out.rows==in.rows && out.cols==in.cols && out.type()==in.type());
     return out;
