05ab7a083a39c7b4812bbcddb063ebbe3c685065
mayumar <maytalbada@gmail.com>
2023-11-08 14:01:31 +0100

    P3 ej2 funcionan 10 test

--- practica3/sharpening/common_code.cpp
+++ practica3/sharpening/common_code.cpp
@@ -11,7 +11,18 @@ fsiv_create_gaussian_filter(const int r)
     // TODO 
     // Remember: 6*sigma is approx 99,73% of the distribution.
 
+    std::vector<int> sizes = {2*r+1, 2*r+1};
+    ret_v = cv::Mat(sizes, CV_32FC1);
 
+    float sigma = (2*r+1)/6.0;
+    for(int i = 0; i < 2*r+1; i++){
+        for(int j = 0; j < 2*r+1; j++){
+            float eq = (1.0/(sigma*sigma*2.0*M_PI)) * exp(-(pow(i-r,2)+pow(j-r,2))/(2.0*sigma*sigma));
+            ret_v.at<float>(cv::Point(i,j)) = eq;
+        }
+    }
+
+    ret_v /= cv::sum(ret_v);
 
     //
     CV_Assert(ret_v.type()==CV_32FC1);
@@ -29,6 +40,24 @@ fsiv_extend_image(const cv::Mat& img, const cv::Size& new_size, int ext_type)
     //TODO
     //Hint: use cv::copyMakeBorder()
 
+    int rh = (new_size.height-img.rows)/2;
+    int rw = (new_size.width-img.cols)/2;
+    out = cv::Mat::zeros(new_size.height, new_size.width, img.type());
+
+    if(ext_type == 1){
+        img(cv::Rect(0,img.rows-rh,img.cols,rh)).copyTo(out(cv::Rect(rw, 0, img.cols,rh)));
+        img(cv::Rect(0,0,img.cols,rh)).copyTo(out(cv::Rect(rw,out.rows-rh, img.cols,rh)));
+
+        img(cv::Rect(img.cols-rw,0,rw,img.rows)).copyTo(out(cv::Rect(0,rh,rw,img.rows)));
+        img(cv::Rect(0,0,rw,img.rows)).copyTo(out(cv::Rect(out.cols-rw,rh,rw,img.rows)));
+
+        img(cv::Rect(img.cols-rw,img.rows-rh,rw,rh)).copyTo(out(cv::Rect(0,0,rw,rh)));
+        img(cv::Rect(0,img.rows-rh,rw,rh)).copyTo(out(cv::Rect(out.cols-rw,0,rw,rh)));
+        img(cv::Rect(img.cols-rw,0,rw,rh)).copyTo(out(cv::Rect(0,out.rows-rh,rw,rh)));
+        img(cv::Rect(0,0,rw,rh)).copyTo(out(cv::Rect(out.cols-rw,out.rows-rh,rw,rh)));
+    }
+
+    img.copyTo(out(cv::Rect(rw, rh, img.cols, img.rows)));
     //
     CV_Assert(out.type()==img.type());
     CV_Assert(out.rows == new_size.height);
@@ -46,7 +75,25 @@ fsiv_create_sharpening_filter(const int filter_type, int r1, int r2)
     // Remember DoG = G[r2]-G[r1].
     // Hint: use fsiv_extend_image() to extent G[r1].
     // Remember: Use -Laplacian + [1] as filter.
+    if(filter_type == 0){
+        filter = cv::Mat::zeros(3, 3, CV_32FC1);
+        filter.at<float>(1,0) = -1;
+        filter.at<float>(0,1) = -1;
+        filter.at<float>(1,1) = 5;
+        filter.at<float>(1,2) = -1;
+        filter.at<float>(2,1) = -1;
+    }else if(filter_type == 1){
+        filter = cv::Mat(3, 3, CV_32FC1, -1);
+        filter.at<float>(1,1) = 9;
+    }else{
+        auto G1 = fsiv_create_gaussian_filter(r1);
+        auto G2 = fsiv_create_gaussian_filter(r2);
 
+        G1 = fsiv_extend_image(G1, G2.size(), 0);
+        filter = G2-G1;
+        filter = -filter;
+        filter.at<float>(filter.rows/2, filter.cols/2)++;
+    }
 
     //
     CV_Assert(filter.type()==CV_32FC1);
@@ -71,7 +118,21 @@ fsiv_image_sharpening(const cv::Mat& in, int filter_type, bool only_luma,
     // Hint: use cv::filter2D using cv::BORDER_ISOLATED.
     // Remember: if circular, first the input image must be circular extended,
     //  and then clip the result.
+    if(!only_luma){
+        cv::Mat in_ext;
+        in.copyTo(in_ext);
 
+        if(circular){
+            in_ext = fsiv_extend_image(in, cv::Size(in.cols+(2*r2), in.rows+(2*r2)), 1);
+        }else{
+            in_ext = fsiv_extend_image(in, cv::Size(in.cols+(2*r2), in.rows+(2*r2)), 0);
+        }
+
+        auto filter = fsiv_create_sharpening_filter(filter_type, r1, r2);
+        cv::Mat out_ext;
+        cv::filter2D(in_ext, out_ext, -1, filter, cv::Point(-1,-1), (0.0), cv::BORDER_ISOLATED);
+        out = out_ext(cv::Rect(r2, r2, in.cols, in.rows));
+    }
 
     //
     CV_Assert(out.type()==in.type());
